be5d3ded5b838266e64cdf8feb1f080bbda7ba17
ernest <ernest.marcinko@gmail.com>
2023-06-08 23:33:32 +0200

    gtag appended

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,6 +53,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-F87BC24RBG',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
